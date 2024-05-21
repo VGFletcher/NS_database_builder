67abf0f394fadccf715b6ada992c5bed12296797
Vincent Fletcher <msuvsb@hetmathsys1.scrtp.warwick.ac.uk>
2024-05-21 15:35:33 +0100

    created partition function xrd calculator

---
+++ part_f_xrd.py
@@ -0,0 +1,134 @@
+#xrd calculator for pymatnest authored by @V.G.Fletcher
+#The purpose is to take the trajectories outputted from a nested sampling run and create a single file that contains...
+#...the xrd data of every configuration in iteration order
+
+import time
+import numpy as np
+from scipy import constants as sc
+from mpi4py import MPI
+import argparse
+
+Kb = sc.physical_constants['Boltzmann constant in eV/K'][0]
+
+def read_energies(file_name):
+    f = open(file_name)
+
+    params = f.readline().strip().split()
+    walkers = int(params[0])
+
+    energies = []
+    for line in f:
+        i, ene, vol = line.strip().split()
+        energies.append(float(ene))
+    return walkers, np.array(energies)
+
+def log_weights(n_Es, K):
+    i_vals = np.arange(0, n_Es, 1)
+    
+    log_X = np.zeros_like(i_vals) + np.log(K/(K+1))
+    log_Xn = np.cumsum(log_X)
+    
+    log_w = log_Xn - np.log(K+1)
+    return log_w
+
+def Z_vals(B, log_weights, energies):
+    log_z = log_weights - (energies * B)
+    shift = np.max(log_z)
+    
+    Z = np.exp(log_z-shift)[10::10]
+    sum_z = np.sum(Z)
+    return Z, sum_z
+    
+#Set up MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+#Parse args
+parser = argparse.ArgumentParser(description='Create temperature weighted XRD data from NS')
+
+parser.add_argument('-i', '--energies_file', action='store', help="Name of the NS energies file", type=str, required=True)
+parser.add_argument('-xrd', '--xrd_data', action='store', help="Name of the file with the xrd data", type=str, required=True)
+
+parser.add_argument('-ti', '--starting_temp', action='store', help="Starting temp for analysis", type=float, required=True)
+parser.add_argument('-tf', '--final_temp', action='store', help="Final temp for analysis", type=float, required=True)
+parser.add_argument('-dt', '--temp_step', action='store', help="Temperature step for analysis", type=float, required=True)
+
+parser.add_argument('-o', '--res_prefix', action='store', help="Prefix of files to save xrd data to", type=str, required=True)
+
+parser.add_argument('-V', '--verb', action='store_true', help="Verbosity of search")
+
+args = parser.parse_args()
+
+energies_file = args.energies_file
+xrd_file = args.xrd_data
+
+start_t = args.starting_temp
+final_t = args.final_temp
+delta_t = args.temp_step
+
+res_prefix = args.res_prefix
+
+verbose   = args.verb
+
+t_vals = np.arange(start_t, final_t, delta_t)
+
+num_t_vals = len(t_vals)
+
+#Divide work between threads
+t_per_thread = round(num_t_vals/size)
+starting_t = t_per_thread * rank
+
+#After even division, give any remaining jobs to the last thread
+if rank == (size-1):
+    extra_t = num_t_vals - (t_per_thread * size)
+    t_per_thread += extra_t
+
+if verbose:
+    print(f"Rank {rank} starting from {start_t} K to {final_t} K with step {delta_t} K")
+
+#Read the energies file
+K, energies = read_energies(energies_file)
+if verbose and rank==0:
+    print('Read energies file')
+    
+#Get the number of iterations
+n_Es = len(energies)
+
+#Calculate the temperature independent weights
+log_w = log_weights(n_Es, K)
+if verbose and rank==0:
+    print('Calculated t independent weights')
+    
+#Import the xrd data
+xrd_dat = np.loadtxt(xrd_file)
+if verbose and rank==0:
+    print('Loaded XRD data')
+
+res_file = open(f"{res_prefix}.{rank}.npy", 'w')
+#Loop through the thread allcoated temperatures
+for i in range(t_per_thread):
+    it = starting_t + i
+    T = t_vals[it]
+    print(T)
+    
+    #Calcualte the Z values for each temp
+    B = 1.0/(Kb*T)
+    Z, sum_Z = Z_vals(B, log_w, energies)
+    
+    weighted_xrd = np.mean(xrd_dat[1:] * np.reshape(Z, (len(Z),1)), axis=0)
+    np.savetxt(res_file, np.reshape(weighted_xrd, (1, len(weighted_xrd))))
+res_file.close()
+comm.barrier()
+
+#Use root thread to concat files
+if rank == 0:
+    master_xrd = open(f"{res_prefix}.npy", 'w')
+    np.savetxt(master_xrd, np.reshape(xrd_dat[0], (1,len(xrd_dat[0]))))
+    for i in range(size):
+        thread_xrd_dat = np.loadtxt(f"{res_prefix}.{i}.npy")
+        np.savetxt(master_xrd, thread_xrd_dat)
+        print(f"concatenated rank {i} data")
+    master_xrd.close()
+comm.barrier()
+    
