edde90357bbf262845e9fbc070e797dcc2d0946b
Vincent Fletcher <msuvsb@login01.avon.hpc>
2024-05-22 18:18:33 +0100

    changed PE to enthalpy

--- part_f_xrd.py
+++ part_f_xrd.py
@@ -17,7 +17,12 @@ def read_energies(file_name):
     energies = []
     iterations = []
     for struc in strucs:
-        energies.append(struc.info['ns_energy'])
+        PE = struc.info['ns_energy']
+        PR = struc.info['ns_P']
+        VO = struc.info['volume']
+
+        enth = PE + (PR * VO)
+        energies.append(enth)
         iterations.append(struc.info['iter'])
     
     return np.array(iterations), np.array(energies)
