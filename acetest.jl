5893ca4cffc52f8266d9bca7946b7f8632fa0c80
Vincent Fletcher <msuvsb@login01.avon.hpc>
2024-05-28 14:40:19 +0100

    created acetest and weighted acetest

---
+++ acetest.jl
@@ -0,0 +1,97 @@
+#Code authored by @V.G.Fletcher for the purpose of evaluating the uncertainty of a set of...
+#...nested sampling configurations
+using Distributed
+
+@everywhere import Pkg
+
+##########################
+#####First time setup#####
+##########################
+#Pkg.Registry.add("General")
+#Pkg.Registry.add(Pkg.RegistrySpec(url="https://github.com/ACEsuit/ACEregistry"))
+#Pkg.add("ACEpotentials")
+#Pkg.add("JLD2")
+#Pkg.add("Glob")
+#Pkg.add("ArgParse")
+##########################
+##########################
+
+############################
+#####IMPORTING_PACKAGES#####
+############################
+@everywhere using ACEpotentials
+@everywhere using JLD2
+@everywhere using Statistics
+using Glob
+using ArgParse
+############################
+############################
+
+###########################
+#####IMPORT_PARAMETERS#####
+###########################
+s = ArgParseSettings()
+@add_arg_table s begin
+    "--traj_regex", "-i"
+    help = "The regex used to identify all traj files in extxyz format"
+    arg_type = AbstractString
+    required = true
+    
+    "--ace_model", "-m"
+    help = "The trained ACE model in .jld2 format"
+    arg_type = AbstractString
+    required = true
+end
+
+parsed_args = parse_args(ARGS, s)
+
+files = Glob.glob(parsed_args["traj_regex"])
+model = load_object(parsed_args["ace_model"])
+
+poten = model.potential
+###########################
+###########################
+
+#######################
+#####PARALLEL_LOOP#####
+#######################
+@sync @distributed for f in files
+   
+    println("Loading $f")
+    dataset = read_extxyz(f)
+
+    num_configs = length(dataset)
+    println("Loaded $f with $num_configs configs")
+
+    eval_configs = Array{Atoms{Float64}, 1}()
+    for j in range(start=1, stop=num_configs, step=1)
+
+    	config = deepcopy(dataset[1])
+	
+	E, E_co = ACE1.co_energy(poten, config)
+        sigma = sqrt(mean((E_co .- E).^2))
+
+	dataset[1]["sigma"] = sigma
+	dataset[1]["ACE_energy"] = E
+
+	push!(eval_configs, dataset[1])
+	deleteat!(dataset, 1)
+	
+	if (j%1000) == 0
+	   GC.gc()
+	end
+    end
+    
+    write_extxyz(f * ".tmp", eval_configs)
+
+    mv(f, f * ".old")
+    mv(f * ".tmp", f)
+    rm(f * ".old")
+    
+    dataset = nothing
+    eval_configs = nothing
+
+    GC.gc()
+end
+#######################
+#######################
---
+++ acetest_T.jl
@@ -0,0 +1,99 @@
+#Code authored by @V.G.Fletcher for the purpose of evaluating the weighted uncertainty of a...
+#...set of nested sampling configurations
+using Distributed
+
+@everywhere import Pkg
+
+##########################
+#####First time setup#####
+##########################
+#Pkg.Registry.add("General")
+#Pkg.Registry.add(Pkg.RegistrySpec(url="https://github.com/ACEsuit/ACEregistry"))
+#Pkg.add("ACEpotentials")
+#Pkg.add("JLD2")
+#Pkg.add("Glob")
+#Pkg.add("ArgParse")
+##########################
+##########################
+
+############################
+#####IMPORTING_PACKAGES#####
+############################
+@everywhere using ACEpotentials
+@everywhere using JLD2
+using Glob
+using ArgParse
+############################
+############################
+
+###########################
+#####IMPORT_PARAMETERS#####
+###########################
+s = ArgParseSettings()
+@add_arg_table s begin
+    "--traj_regex", "-i"
+    help = "The regex used to identify all traj files in extxyz format"
+    arg_type = AbstractString
+    required = true
+    
+    "--ace_model", "-m"
+    help = "The trained ACE model in .jld2 format"
+    arg_type = AbstractString
+    required = true
+end
+
+parsed_args = parse_args(ARGS, s)
+
+files = Glob.glob(parsed_args["traj_regex"])
+model = load_object(parsed_args["ace_model"])
+
+poten = model.potential
+###########################
+###########################
+
+#######################
+#####PARALLEL_LOOP#####
+#######################
+@sync @distributed for f in files
+    Kb = 8.61733326 * 10^-5 #eV/K
+   
+    println("Loading $f")
+    dataset = read_extxyz(f)
+
+    num_configs = length(dataset)
+    println("Loaded $f with $num_configs configs")
+
+    eval_configs = Array{Atoms{Float64}, 1}()
+    for j in range(start=1, stop=num_configs, step=1)
+
+    	config = deepcopy(dataset[1])
+
+	sigma = config.data["sigma"].data
+
+	temp = config.data["temp"].data
+	if temp != "-inf" && temp != "inf" && !isnan(temp)
+	   w_sigma = 1 - exp(-sigma/(Kb*temp))
+	   dataset[1]["w_sigma"] = w_sigma
+	end
+	
+	push!(eval_configs, dataset[1])
+	deleteat!(dataset, 1)
+	
+	if (j%1000) == 0
+	   GC.gc()
+	end
+    end
+    
+    write_extxyz(f * ".tmp", eval_configs)
+
+    mv(f, f * ".old")
+    mv(f * ".tmp", f)
+    rm(f * ".old")
+    
+    dataset = nothing
+    eval_configs = nothing
+
+    GC.gc()
+end
+#######################
+#######################
