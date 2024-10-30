443c6e1f4ce4247d2316b63c02bc86e0007b94bd
VGFletcher <vincent.fletcher@warwick.ac.uk>
2024-10-30 18:48:38 +0000

    added energy based weighting

--- ACE_UQ/acefit.jl
+++ ACE_UQ/acefit.jl
@@ -20,6 +20,7 @@ import Pkg
 ############################
 #####IMPORTING_PACKAGES#####
 ############################
+using LinearAlgebra
 using ACEpotentials
 using Suppressor
 using Random
@@ -51,6 +52,9 @@ n_members = 10
 repul_rest = false
 repul_weight = 0.0
 
+#0.5 is too extreme, 0.2 is reasonable
+#higher numbers reduce importance of high temp configs
+alpha = 0.4
 #####################
 #####################
 
@@ -65,8 +69,6 @@ model = acemodel(elements = elements,
                     order = order,
               totaldegree = degree,
                      Eref = erefs)
-
-weights = Dict( "default" => Dict("E" => e_weight, "F" => f_weight, "V" => v_weight))
 
 #prior = smoothness_prior(model)
 solver = ACEfit.BLR(tol=blr_tol, committee_size=n_members, factorization=:svd)
@@ -89,6 +91,55 @@ println("TEST_SET_HAS_", test_size, "_CONFIGURATIONS\n")
 shuffle!(dataset)
 test_db = dataset[1:test_size]
 train_db = dataset[test_size+1:ds_size]
+
+##########################
+##########################
+
+##########################
+#####Creating Weights#####
+##########################
+pressure_sets = Dict()
+for (i, at) in enumerate(train_db)
+    press = at.data["ns_P"].data
+    at.data["config_type"].data = "con_" * string(i)
+    pressure_sets[press] = [[],[]]
+end
+
+#Calculate the DFT enthalpy from the DFT potential energy
+for at in train_db
+    press = at.data["ns_P"].data
+    i = at.data["config_type"].data
+    
+    pe = at.data["dft_energy"].data
+    cell_v = det(cell(at))
+    pv = cell_v * press
+    H = pe + pv
+    push!(pressure_sets[press][1], i)
+    push!(pressure_sets[press][2], H)
+end
+
+#Rescale all the sets to their lowest enthalpy configuration
+for p_set in keys(pressure_sets)
+    enes = pressure_sets[p_set][2]
+    E0 = enes[argmin(enes)]
+    scaled_ene = enes .- E0
+    pressure_sets[p_set][2] = scaled_ene
+end
+
+#Calculate the scaling factors through sf = exp(-alpha * (E-E0))
+for p_set in keys(pressure_sets)
+    enes = pressure_sets[p_set][2]
+    sfs = exp.(-alpha .* enes)
+    pressure_sets[p_set][2] = sfs
+end
+
+#Create a dictionary of the rescaled weights
+weights = Dict()
+for p_set in keys(pressure_sets)
+    for (i,w) in zip(pressure_sets[p_set][1],pressure_sets[p_set][2])
+        weights[i] = Dict("E" => e_weight * w, "F" => f_weight * w, "V" => v_weight * w)
+    end
+end
 
 ##########################
 ##########################
